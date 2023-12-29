6eb9facf06d512de701a410ffe5783eb8ae58395
allenash0126 <allen64031@hotmail.com>
2023-12-29 17:12:32 +0800

    install & initialize sequelize

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const db = require('./models')
+const Restaurant = db.Restaurant
+
 app.get('/',(req,res) => {
   res.send('Server is listening')
+})
+
+app.get('/restaurants',(req,res) => {
+  return Restaurant.findAll()
+    .then((restaurants) => res.send({ restaurants }))
 })
 
 app.listen(port,() => {
