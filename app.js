e82db7e9d2c284f78ec0487a1af673391f41fc9d
allenash0126 <allen64031@hotmail.com>
2023-12-30 11:06:32 +0800

    initialize handlebars layout

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
 const db = require('./models')
 const Restaurant = db.Restaurant
 
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
 app.get('/',(req,res) => {
-  res.send('Server is listening')
+  res.render('index')
 })
 
 app.get('/restaurants',(req,res) => {
