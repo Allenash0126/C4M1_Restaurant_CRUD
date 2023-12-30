2866660ad2334411404d2979e8711a01b66432ca
allenash0126 <allen64031@hotmail.com>
2023-12-30 15:43:30 +0800

    implement get restaurant list

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+// const restaurants = require('./public/jsons/restaurant.json').results
 
 const db = require('./models')
 const Restaurant = db.Restaurant
@@ -12,12 +13,24 @@ app.set('views', './views');
 app.use(express.static('public'))
 
 app.get('/',(req,res) => {
-  res.render('index')
+  const keyword = req.query.keyword?.trim()
+  const matchedRestaurants = keyword ? restaurants.filter(rt => 
+    Object.values(rt).some((property) => {
+      if (typeof property === 'string') {
+      return property.toLowerCase().includes(keyword.toLowerCase())
+      }
+      return false
+    })    
+  ) : restaurants
+  res.render('index',{ restaurants: matchedRestaurants, keyword })
 })
 
 app.get('/restaurants',(req,res) => {
-  return Restaurant.findAll()
-    .then((restaurants) => res.send({ restaurants }))
+  return Restaurant.findAll({
+    attributes:['id','name','name_en','category','image','location','phone','google_map','rating','description'],
+    raw: true
+  })
+    .then((restaurants) => res.render('restaurants',{ restaurants }))
 })
 
 app.listen(port,() => {
