13fabac7e85cb30472438268a6bb0688c2a36bd5
allenash0126 <allen64031@hotmail.com>
2024-01-16 15:55:18 +0800

    add flash messages

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const flash = require('connect-flash')
+const session = require('express-session')
 const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
 const app = express()
@@ -15,6 +17,12 @@ app.set('views', './views');
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsSecret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
 
 app.get('/',(req,res) => {
   res.redirect('/restaurants')
@@ -35,7 +43,7 @@ app.get('/restaurants',(req,res) => {
           return false
         })    
       ) : restaurants
-      res.render('restaurants',{ restaurants: matchedRestaurants, keyword })})
+      res.render('restaurants',{ restaurants: matchedRestaurants, keyword, message: req.flash('success') })})
 })
 
 app.get('/restaurants/new',(req,res) => {
@@ -55,7 +63,9 @@ app.post('/restaurants', (req,res) => {
   const description = req.body.description
 
   return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating,description })
-  .then(() => res.redirect('/restaurants'))
+  .then(() => {
+    req.flash('success','新增成功！')
+    res.redirect('/restaurants')})
 })
 
 app.get('/restaurants/:id',(req,res) => {
@@ -65,7 +75,7 @@ app.get('/restaurants/:id',(req,res) => {
     attributes:['id','name','name_en','category','image','location','phone','google_map','rating','description'],
     raw: true
   })
-    .then((restaurant) => res.render('restaurant', {restaurant}))
+    .then((restaurant) => res.render('restaurant', { restaurant, message: req.flash('success') }))
 })
 
 // 進入編輯頁面，並提取已在DB中的資料，放到既有欄位中
@@ -85,13 +95,17 @@ app.put('/restaurants/:id',(req,res) => {
   const id = req.params.id
 
   return Restaurant.update({ name:body.name, name_en:body.name_en, category:body.category, image:body.image, location:body.location, phone:body.phone, google_map:body.google_map, rating:body.rating, description:body.description}, { where:{id} })
-    .then(() => res.redirect(`/restaurants/${id}`))
+    .then(() => {
+      req.flash('success','更新成功！')
+      res.redirect(`/restaurants/${id}`)})
 })
 
 app.delete('/restaurants/:id', (req,res) => {
   const id = req.params.id
   return Restaurant.destroy({ where: {id} })
-    .then(() => res.redirect('/restaurants'))  
+    .then(() => {
+      req.flash('success','刪除成功！')
+      res.redirect('/restaurants')})  
 })
 
 app.listen(port,() => {
