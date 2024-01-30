2357f3f57ba141455dcbcd57e93d4078142a89c7
allenash0126 <allen64031@hotmail.com>
2024-01-30 16:47:00 +0800

    add basic structure for register

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const router = express.Router()
 const restaurants = require('./restaurants')
+const users = require('./users')
 
 // (1) 這裡把restaurants.js當作是middleware來引用
 // (2) 前面的'/restaurants'，表示未來在restaurants.js中的預設新路徑 都是restaurants開頭，避免兩個路徑模組都有/根目錄 衝突
 // (3) 記得去restaurants.js，把路徑/restaurants刪掉，避免路徑變成/restaurants/restaurants...
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 router.get('/', (req, res) => {
   res.redirect('/restaurants')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', (req, res) => {
+  const loginInfo = req.body
+  console.log(loginInfo.email, loginInfo.password)
+})
+
+router.get('/logout', (req, res) => {
+  res.send('You have logged out. Bye~')
 })
 
 module.exports = router
---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+const User = db.User
+
+router.post('/', (req, res, next) => {
+  const name = req.body.name
+  const email = req.body.email
+  const password = req.body.password
+  const confirmPassword = req.body.confirmPassword
+
+  if (password !== confirmPassword) {
+    req.flash('fail', '輸入兩次不同的密碼 :(')
+    return res.redirect('register')
+  }
+
+  User.findAll({
+    attributes: ['id', 'name', 'email', 'password'],
+    raw: true
+  })
+    .then((users) => {
+      const isEmailExists = users.find((user) => user.email === email)
+      if (isEmailExists) {
+        req.flash('fail', '已註冊，請直接登入！')
+        return res.redirect('login')
+      }
+      return User.create({ name, email, password })
+        .then(() => {
+          req.flash('success', '註冊成功！')
+          res.redirect('login')
+        })
+        .catch((error) => {
+          req.errorMessage = '註冊失敗:('
+          next(error)
+        })
+    })
+})
+
+module.exports = router
