93e564fe42d71b295559b052caa09e3a34d9c7a6
allenash0126 <allen64031@hotmail.com>
2024-01-30 19:29:52 +0800

    implement auth check using passport

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,6 @@ const LocalStrategy = require('passport-local')
 const db = require('../models')
 const User = db.User
 
-const restaurants = require('./restaurants')
-const users = require('./users')
-
-// (1) 這裡把restaurants.js當作是middleware來引用
-// (2) 前面的'/restaurants'，表示未來在restaurants.js中的預設新路徑 都是restaurants開頭，避免兩個路徑模組都有/根目錄 衝突
-// (3) 記得去restaurants.js，把路徑/restaurants刪掉，避免路徑變成/restaurants/restaurants...
-router.use('/restaurants', restaurants)
-router.use('/users', users)
-
 passport.use(new LocalStrategy({ usernameField: 'email' }, (username, password, done) => {
   return User.findOne({
     attributes: ['id', 'name', 'email', 'password'],
@@ -37,6 +28,20 @@ passport.serializeUser((user, done) => {
   return done(null, { id, name, email })
 })
 
+passport.deserializeUser((user, done) => {
+  done(null, { id: user.id })
+})
+
+const restaurants = require('./restaurants')
+const users = require('./users')
+const authHandler = require('../middlewares/auth-handler')
+
+// (1) 這裡把restaurants.js當作是middleware來引用
+// (2) 前面的'/restaurants'，表示未來在restaurants.js中的預設新路徑 都是restaurants開頭，避免兩個路徑模組都有/根目錄 衝突
+// (3) 記得去restaurants.js，把路徑/restaurants刪掉，避免路徑變成/restaurants/restaurants...
+router.use('/restaurants', authHandler, restaurants)
+router.use('/users', users)
+
 router.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
@@ -55,8 +60,13 @@ router.post('/login', passport.authenticate('local', {
   failureFlash: true
 }))
 
-router.get('/logout', (req, res) => {
-  res.send('You have logged out. Bye~')
+router.get('/logout', (req, res, next) => {
+  req.logout((error) => {
+    if (error) {
+      next(error)
+    }
+    return res.redirect('/login')
+  })
 })
 
 module.exports = router
