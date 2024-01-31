fd283d156f15bdd7e487c0cb27d731677a7dccc4
allenash0126 <allen64031@hotmail.com>
2024-01-31 16:56:42 +0800

    add bcrypt for seeder, define userId 1&2 to rt

--- seeders/20231229093824-initial-data.js
+++ seeders/20231229093824-initial-data.js
@@ -1,21 +1,54 @@
 'use strict'
 
-// 由原本手動鍵入種子資料，改為自動載入整筆json
+const bcrypt = require('bcryptjs')
+
+// 應作業要求 前三個是user1，後三個是user2
 const initialData = require('../public/jsons/restaurant.json').results
 initialData.forEach((data) => {
-  // 因為autoIncrement 會自動產生id，所以這裡先移除id 不然會跳錯
-  delete data.id
   data.createdAt = new Date()
   data.updatedAt = new Date()
+  if (data.id < 4) data.userId = 1
+  else data.userId = 2
+  // 因為autoIncrement 會自動產生id，所以這裡先移除id 不然會跳錯
+  delete data.id
 })
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Restaurants', initialData)
+    let transaction
+
+    try {
+      transaction = await queryInterface.sequelize.transaction()
+      const hashPwd = await bcrypt.hash('123456768', 10)
+      await queryInterface.bulkInsert('Users', [
+        {
+          id: 1,
+          name: 'user1',
+          email: 'user1@example.com',
+          password: hashPwd,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }, {
+          id: 2,
+          name: 'user2',
+          email: 'user2@example.com',
+          password: hashPwd,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ], { transaction }
+      )
+      await queryInterface.bulkInsert('Restaurants', initialData, { transaction })
+      await transaction.commit()
+    } catch (error) {
+      if (transaction) await transaction.rollback()
+    }
+    // 以下原始code
+    // await queryInterface.bulkInsert('Restaurants', initialData)
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Restaurants', null)
+    await queryInterface.bulkDelete('Users', null)
   }
 }
